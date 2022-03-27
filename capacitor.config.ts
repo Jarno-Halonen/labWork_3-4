9142b43fc734230706a722d7284fbc0471dadba3
Jarno Halonen <jarno.halonen@outlook.com>
2022-03-27 13:28:14 +0300

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'gmailClone',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
